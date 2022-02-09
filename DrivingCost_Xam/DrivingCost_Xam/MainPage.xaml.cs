81b1031d7ce1ed586e05104eb579c55a7bcead8e
Marcin <Marcin@LAPTOP-MZ-W11>
2022-02-09 18:44:06 +0100

    Add project files.

---
+++ DrivingCost_Xam/DrivingCost_Xam/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DrivingCost_Xam
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }  
+        
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            double Fuel_Price = Convert.ToDouble(EntryFuelPrice.Text);
+            double Fuel_Consumption = Convert.ToSingle(EntryFuelConsumption.Text);
+            double Route_Length = Convert.ToSingle(EntryRouteLength.Text);
+
+            double Driving_Cost = (Convert.ToSingle(Fuel_Price * Fuel_Consumption) / 100) * Route_Length;
+
+            
+            
+            lbl_Result.Text = Convert.ToString(Math.Round(Driving_Cost, 2) + " PLN");
+            lbl_Result.TextColor = Color.Red;
+        }
+
+        void ButtonClicked(object sender, EventArgs e)
+        {
+            lbl_Result.Text = "Cost";
+            lbl_Result.TextColor = Color.Gray;
+
+            EntryFuelPrice.Text = "";
+            EntryFuelConsumption.Text = "";
+            EntryRouteLength.Text = "";
+        }
+
+        private void EntryFuelPrice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //btnCalculate.IsEnabled = false;
+
+            if (EntryFuelPrice.Text.Length > 0)
+            {
+                btnCalculate.IsEnabled = true;
+                btnClear.IsEnabled = true;
+            }             
+            else
+            {
+                btnCalculate.IsEnabled = false;
+                btnClear.IsEnabled = false;
+            }
+        }
+        private void EntryFuelCons_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //btnCalculate.IsEnabled = false;
+
+            if (EntryFuelConsumption.Text.Length > 0)
+            {
+                btnCalculate.IsEnabled = true;
+                btnClear.IsEnabled = true;
+            }
+            else
+            {
+                btnCalculate.IsEnabled = false;
+                btnClear.IsEnabled = false;
+            }
+        }
+        private void EntryRouteLength_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //btnCalculate.IsEnabled = false;
+
+            if (EntryRouteLength.Text.Length > 0)
+            {
+                btnCalculate.IsEnabled = true;
+                btnClear.IsEnabled = true;
+            }
+            else
+            {
+                btnCalculate.IsEnabled = false;
+                btnClear.IsEnabled = false;
+            }
+
+        }
+    }
+}
